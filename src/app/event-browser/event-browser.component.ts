f509da5275b44abfd7e8b4ce20d883f76c815b9e
Vincent Marmin <vincent.marmin@davidson.fr>
2017-01-29 11:05:16 +0100

    fix(browser): fix loading event and track from a complet URL

--- src/app/event-browser/event-browser.component.ts
+++ src/app/event-browser/event-browser.component.ts
@@ -1,10 +1,15 @@
-import {Component, ViewChild, ElementRef, OnInit, OnDestroy} from '@angular/core';
+import {Component, ViewChild, ElementRef, OnInit} from '@angular/core';
 import {JsrolService} from '../services/jsrol.service';
-import {Params, ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import * as _ from 'lodash';
-import {Observable, Subscription, BehaviorSubject} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
 import 'material-design-lite/material.js';
 import moment = require('moment');
+
+interface EventBrowserParams{
+  eventId: string;
+  trackId: string;
+}
 
 @Component({
   providers: [JsrolService],
@@ -13,100 +18,70 @@ import moment = require('moment');
   ],
   template: require('./event-browser.component.html')
 })
-export class EventBrowserComponent implements OnInit, OnDestroy {
+export class EventBrowserComponent implements OnInit {
   @ViewChild('mdlLayout') mdlLayout: ElementRef;
   event$ = new BehaviorSubject<EventModel>({});
   tracks$ = new BehaviorSubject<TrackModel[]>([]);
   currentTrack$ = new BehaviorSubject<TrackModel>({});
 
-  eventSubscription: Subscription;
-  tracksSubscription: Subscription;
-  currentTrackSubscription: Subscription;
+  static fromDate: number = moment('2016-07-01').valueOf();
 
   constructor(private jsRolService: JsrolService, private route: ActivatedRoute, private router: Router) {
 
   }
 
   ngOnInit() {
-    this.eventSubscription = this.loadEvents();
-    this.tracksSubscription = this.loadTracks();
-    this.currentTrackSubscription = this.loadCurrentTrack();
-  }
-
-  ngOnDestroy() {
-    this.eventSubscription.unsubscribe();
-    this.tracksSubscription.unsubscribe();
-    this.currentTrackSubscription.unsubscribe();
-  }
-
-  private loadEvents(): Subscription {
-    const fromDate: number = moment('2016-07-01').valueOf();
-
-    // EVENTS
-    return this.route.queryParams
-      .flatMap((params: Params): Observable<Object> => {
-        const eventId: string = params['eventId'];
-        if (!eventId) {
-          return this.jsRolService.getEvents(fromDate, 1)
-            .map((events: EventModel[]) => {
-              if (!_.isEmpty(events)) {
-                this.router.navigate([''], {
-                  queryParams: {
-                    eventId: events[0].$key
-                  }
-                });
-              }
-              return null;
-            });
+    this.route.queryParams
+      .subscribe((params: EventBrowserParams) => {
+        if(!params.eventId){
+          this.loadEventAndRedirect();
+        } else if(params.eventId && !params.trackId){
+          this.loadTracksAndRedirect(params.eventId);
         } else {
-          return this.jsRolService.getEvent(eventId);
+          this.loadEventAndTrack(params.eventId, params.trackId);
         }
-      })
-      .subscribe({
-        next: (event: EventModel) => {
-          if (!event) {
-            return;
-          }
+      });
+  }
+
+  loadEventAndRedirect(){
+    this.jsRolService.getEvents(EventBrowserComponent.fromDate, 1)
+      .subscribe((events: EventModel[]) => {
+        if (!_.isEmpty(events)) {
+          const event = events[0];
+          this.router.navigate([''], {
+            queryParams: {
+              eventId: event.$key,
+            }
+          });
+        }
+      });
+  }
+
+  loadTracksAndRedirect(eventId:string){
+    this.jsRolService.getEvent(eventId)
+      .subscribe((event) => {
+        if(!event.loop1){
           this.event$.next(event);
-        },
-        error: (err) => {
-          console.log(err);
+          this.tracks$.next([]);
+          this.currentTrack$.next(null);
         }
-      });
+
+        this.router.navigate([''], {
+          queryParams: {
+            eventId: eventId,
+            trackId: event.loop1
+          }
+        })
+    });
   }
 
-  private loadTracks(): Subscription {
-    // TRACKS
-    return this.event$
-      .flatMap(this.jsRolService.getEventLoops.bind(this.jsRolService))
-      .subscribe({
-        next: (loops: TrackModel[]) => {
-          this.tracks$.next(loops);
-          this.currentTrack$.next(loops[0]);
-        },
-        error: (err) => {
-          console.log(err);
-        }
-      });
-  }
-
-  private loadCurrentTrack(): Subscription {
-    return this.route.queryParams
-      .flatMap((params: Params) => {
-        const trackId: string = params['trackId'];
-        if (trackId) {
-          return this.jsRolService.getTrack(trackId);
-        } else {
-          return Observable.empty();
-        }
-      })
-      .subscribe({
-        next: (track: TrackModel) => {
-          this.currentTrack$.next(track);
-        },
-        error: (err) => {
-          console.log(err);
-        }
+  loadEventAndTrack(eventId: string, trackId: string){
+    this.jsRolService.getEvent(eventId)
+      .do(event => this.event$.next(event))
+      .flatMap(event => this.jsRolService.getEventLoops(event))
+      .subscribe((loops: TrackModel[]) => {
+        this.tracks$.next(loops);
+        this.currentTrack$.next(_.find(loops, {$key: trackId}));
       });
   }
 
